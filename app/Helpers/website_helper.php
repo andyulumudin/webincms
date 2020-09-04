3b4ea27395464d08bf6920857bbd09f7f484d8bf
Gunawan Gee <geewebid@gmail.com>
2020-09-04 13:37:49 +0700

    fixing website helper and env for CLI php sparks

--- app/Helpers/website_helper.php
+++ app/Helpers/website_helper.php
@@ -1,36 +1,43 @@
 <?php
 
-if(!function_exists('baseUrl')) {
-    function baseUrl() {
-        $baseUrl = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) : 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
-        return $baseUrl;
-    }
-}
-if(!function_exists('website')) {
-    function website($keyname, $val=false) {
-        $filename = ROOTPATH.'website.json';
-		$website = file_get_contents($filename);
-        $websiteData = json_decode($website, true);
-        if($val !== false) {
-		foreach($websiteData as $key => $web) {
-			if($key == $keyname) {
-				$websiteData[$keyname] = $val;
-			}
-		}
-		$config = json_encode($websiteData, JSON_PRETTY_PRINT);
-        return file_put_contents($filename, $config);
+if (!function_exists('baseUrl')) {
+    function baseUrl()
+    {
+        if (!isset($_SERVER['HTTP_HOST'])) {
+            return false;
         } else {
-        return $websiteData[$keyname];
+            $baseUrl = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) : 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
+            return $baseUrl;
         }
     }
 }
-if(!function_exists('setting')) {
-    function setting($name = false) {
-        if($name !== false) {
+if (!function_exists('website')) {
+    function website($keyname, $val = false)
+    {
+        $filename = ROOTPATH . 'website.json';
+        $website = file_get_contents($filename);
+        $websiteData = json_decode($website, true);
+        if ($val !== false) {
+            foreach ($websiteData as $key => $web) {
+                if ($key == $keyname) {
+                    $websiteData[$keyname] = $val;
+                }
+            }
+            $config = json_encode($websiteData, JSON_PRETTY_PRINT);
+            return file_put_contents($filename, $config);
+        } else {
+            return $websiteData[$keyname];
+        }
+    }
+}
+if (!function_exists('setting')) {
+    function setting($name = false)
+    {
+        if ($name !== false) {
             $db = \Config\Database::connect();
             $data = $db->table('setting')->where('setting_name', $name)->get()->getResultArray();
-            
-            if(count($data) > 0) {
+
+            if (count($data) > 0) {
                 return $data[0]['setting_value'];
             }
         }
